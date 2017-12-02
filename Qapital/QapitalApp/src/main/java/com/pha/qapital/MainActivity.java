b023ce3b97216fc69de265b16c954d6288e416c3
Peter Hansen <peter.hanzen@gmail.com>
2017-12-02 12:03:13 +0100

    Initial project commit

---
+++ Qapital/QapitalApp/src/main/java/com/pha/qapital/MainActivity.java
@@ -0,0 +1,13 @@
+package com.pha.qapital;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
